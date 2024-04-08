a6f5c20a060d0398e394caf10c6496d4052d18d4
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-04-08 16:55:18 +0300

    Implement products home page and filter with query params.

---
+++ src/app/products/page.js
@@ -0,0 +1,30 @@
+import { items } from "@/data.json";
+import ProductCard from "@/components/ProductCard";
+import styles from "@/app/styles/home.module.css";
+import FilterBar from "@/components/FilterBar.jsx";
+import Link from "next/link";
+
+export default function Home({ searchParams }) {
+  const filteredItems = items.filter((item) =>
+    Object.entries(searchParams || {}).every(([key, list]) =>
+      list.split(",").includes(item[key])
+    )
+  );
+
+  return (
+    <main className={styles.container}>
+      <FilterBar />
+      <div className={styles.productsContainer}>
+        {filteredItems.map((item) => (
+          <Link
+            href={`/products/${item.id}`}
+            key={item.id}
+            className={styles.product}
+          >
+            <ProductCard item={item} />
+          </Link>
+        ))}
+      </div>
+    </main>
+  );
+}
