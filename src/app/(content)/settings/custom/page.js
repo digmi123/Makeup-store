c86296b7c4a692c172593174b8d0fb98db6602b0
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-05-07 10:29:38 +0300

    Implement custom page for admins (work in progress).

--- src/app/(content)/settings/custom/page.js
+++ src/app/(content)/settings/custom/page.js
@@ -1,7 +1,27 @@
+import styles from "@/app/styles/profile.module.css";
+import Image from "next/image";
+
 export default function custom() {
   return (
-    <main>
-      <h1>custom</h1>
+    <main className={styles.container}>
+      <div className={styles.bioContainer}>
+        <Image
+          className={styles.profileImage}
+          src={
+            "https://s3.amazonaws.com/donovanbailey/products/api_featured_images/000/000/949/original/data?1514082704"
+          }
+          alt="Image"
+          width={200}
+          height={200}
+        />
+        <div className={styles.bioContent}>
+          <h1 className={styles.name}>L.C.D Construction</h1>
+          <p className={styles.description}>
+            This company is focused on maintaining construction projects all
+            over the world.
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
