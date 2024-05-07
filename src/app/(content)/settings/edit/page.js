099840fc7713d02e671f70ddb6293182f0652f17
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-05-07 10:29:07 +0300

    Implement the edit website page.

--- src/app/(content)/settings/edit/page.js
+++ src/app/(content)/settings/edit/page.js
@@ -1,10 +1,93 @@
-export default function edit() {
+"use client";
+
+import LayoutComponent from "@/components/LayoutComponent";
+import { useTheme } from "@/app/providers/ThemeProvider";
+import styles from "@/app/styles/edit.module.css";
+import { useRef } from "react";
+
+export default function Edit() {
+  const layoutRef = useRef(null);
+  const theme = useTheme();
+
+  const handleChange = (event) => {
+    const { value, name } = event.target;
+    theme.colorElement({ [name]: value }, layoutRef.current);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const colors = {
+      "--background-color": event.target["--background-color"].value,
+      "--text-color": event.target["--text-color"].value,
+    };
+    if (!layoutRef.current) return;
+    theme.colorElement(colors, layoutRef.current);
+  };
+
   return (
-    <main>
-      <h1>Edit</h1>
-      <h1>Edit</h1>
-      <h1>Edit</h1>
-      <h1>Edit</h1>
+    <main className={styles.gridContainer}>
+      <form
+        onSubmit={handleSubmit}
+        className={styles.colorsBar}
+        onChange={handleChange}
+      >
+        <label>
+          background-color
+          <input
+            type="color"
+            name="--background-color"
+            defaultValue={theme.currentColors["--background-color"]}
+          />
+        </label>
+
+        <label>
+          text-color
+          <input
+            type="color"
+            name="--text-color"
+            defaultValue={theme.currentColors["--text-color"]}
+          />
+        </label>
+
+        <label>
+          text-color
+          <input
+            type="color"
+            name="--text-color"
+            defaultValue={theme.currentColors["--text-color"]}
+          />
+        </label>
+
+        <label>
+          text-color
+          <input
+            type="color"
+            name="--text-color"
+            defaultValue={theme.currentColors["--text-color"]}
+          />
+        </label>
+
+        <label>
+          text-color
+          <input
+            type="color"
+            name="--text-color"
+            defaultValue={theme.currentColors["--text-color"]}
+          />
+        </label>
+
+        <label>
+          text-color
+          <input
+            type="color"
+            name="--text-color"
+            defaultValue={theme.currentColors["--text-color"]}
+          />
+        </label>
+        <button>Apply</button>
+      </form>
+
+      <LayoutComponent passedRef={layoutRef} />
     </main>
   );
 }
