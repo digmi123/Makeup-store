34322a54a4fc04e884317bcd06d1a5554a1be14f
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-05-07 10:22:27 +0300

    Add restrictions for pages with matcher inside the middelware.

--- src/middleware.js
+++ src/middleware.js
@@ -1,23 +1,14 @@
 import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
-export default withAuth(
-  function middleware(req) {
-    if (
-      req.nextUrl.pathname === "/users/add" &&
-      req.nextauth.token?.role !== "admin"
-    ) {
-      return new NextResponse("You dont have premission for this path");
-    }
+export default withAuth(function middleware(req) {}, {
+  callbacks: {
+    authorized: ({ token }) => !!token,
   },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token,
-    },
-  }
-);
+});
 
 export const config = {
+  matcher: ["/products/((?!products).*)"],
   pages: {
     signIn: "/auth/login",
   },
