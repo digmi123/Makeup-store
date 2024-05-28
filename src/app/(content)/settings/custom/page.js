e50229044f3b97da77a27c7106ca8a7ad9c0db3b
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-05-28 19:57:57 +0300

    Update the styling to tailwind.

--- src/app/(content)/settings/custom/page.js
+++ src/app/(content)/settings/custom/page.js
@@ -1,25 +1,37 @@
-import styles from "@/app/styles/profile.module.css";
 import Image from "next/image";
+import unicorn_logo from "@/assets/unicorn-logo.svg";
+import star from "@/assets/star.svg";
 
 export default function custom() {
   return (
-    <main className={styles.container}>
-      <div className={styles.bioContainer}>
+    <main className="w-full p-4 grid grid-rows-[min-content_auto]">
+      <div className="flex items-center border-b-2">
         <Image
-          className={styles.profileImage}
-          src={
-            "https://s3.amazonaws.com/donovanbailey/products/api_featured_images/000/000/949/original/data?1514082704"
-          }
+          src={unicorn_logo}
+          className="border-2 rounded-full "
           alt="Image"
           width={200}
           height={200}
         />
-        <div className={styles.bioContent}>
-          <h1 className={styles.name}>L.C.D Construction</h1>
-          <p className={styles.description}>
+        <div className="p-5">
+          <h1>L.C.D Construction</h1>
+          <p>
             This company is focused on maintaining construction projects all
             over the world.
           </p>
+          <div className="flex gap-2 py-4">
+            {Array(5)
+              .fill()
+              .map((_, index) => (
+                <Image
+                  src={star}
+                  alt="Star"
+                  width={20}
+                  height={20}
+                  key={index}
+                />
+              ))}
+          </div>
         </div>
       </div>
     </main>
