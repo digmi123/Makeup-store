0fbad6e2a6b8bfc9d8fbaf21ce4c1056b5848f33
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-05-28 19:57:12 +0300

    Update login page to work with shadcn components and tailwind.

--- src/app/api/auth/login/page.js
+++ src/app/api/auth/login/page.js
@@ -1,7 +1,17 @@
 import { cookies } from "next/headers";
-import styles from "@/app/styles/login.module.css";
+import * as React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 import authOptions from "../[...nextauth]/authOptions";
+import { Input } from "@/components/ui/input";
 
 //making the callback only when the csrfToken is available if there is not csrf token the page callback is jibrish.
 export default async function login() {
@@ -11,40 +21,46 @@ export default async function login() {
   const token = cookieStore.get("next-auth.csrf-token");
   const csrfToken = token?.value.split("|")[0];
   return (
-    <div className={styles.pageContainer}>
-      <h1 className={styles.title}>Makeup yourself</h1>
-      <div className={styles.wrapper}>
-        <form
-          method="post"
-          action="/api/auth/callback/credentials?callbackUrl=/products"
-          className={styles.formContainer}
-        >
-          <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-          <div className={styles.inputWrapper}>
-            <label>Your email</label>
-            <input name="email" type="text" />
-          </div>
+    <div className="w-full min-h-calc(100dvh - var(--header-height)) min-h-calc(100vh - var(--header-height)) flex flex-col items-center justify-center bg-cover bg-no-repeat bg-center h-screen">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Makeup yourself</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form
+            method="post"
+            action="/api/auth/callback/credentials?callbackUrl=/products"
+          >
+            <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <label htmlFor="email">Email</label>
+                <Input
+                  id="email"
+                  placeholder="Email"
+                  name="email"
+                  type="email"
+                />
+              </div>
 
-          <div className={styles.inputWrapper}>
-            <label>Your Password</label>
-            <input name="password" type="password" />
-          </div>
+              <div className="flex flex-col space-y-1.5">
+                <label htmlFor="password">password</label>
+                <Input
+                  id="password"
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                />
+              </div>
+            </div>
 
-          <button className={styles.submitBtn} type="submit">
-            Sign in
-          </button>
-
-          <div>
-            {Object.values(providers).map((provider) => {
-              return (
-                <div key={provider.name}>
-                  <button>Sign in with {provider.name}</button>
-                </div>
-              );
-            })}
-          </div>
-        </form>
-      </div>
+            <CardFooter className="flex justify-between">
+              <Button>Register</Button>
+              <Button type="submit">Sign in</Button>
+            </CardFooter>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
