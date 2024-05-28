502ba5c5aff652561d9ad8c2b01a835583995d88
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-05-28 19:59:17 +0300

    Get Item from db on server component.

--- src/app/(content)/products/[id]/page.js
+++ src/app/(content)/products/[id]/page.js
@@ -1,64 +1,18 @@
 import ImageSelector from "@/components/ImageSelector";
 import RelatedItems from "@/components/RelatedItems";
-import ShippingOptions from "@/components/ShippingOptions";
-import { items } from "@/data.json";
-import styles from "@/app/styles/productPage.module.css";
-import QuantitySection from "@/components/QuantitySection";
-
-const sizeList = ["XL", "L", "M", "S"];
-
-const getProduct = async (productId) => {
-  return items.find((item) => item.id === Number(productId));
-};
+import ProductInformation from "@/components/ProductInformation";
 
 export default async function Product({ params }) {
-  const itemId = params.id;
-  const item = await getProduct(itemId);
+  const item = await prisma.product.findFirst({
+    where: { id: Number(params.id) },
+    include: { productColors: { include: { color: true } } },
+  });
 
   return (
     <div style={{ width: "100%" }}>
-      <div className={styles.productContainer}>
-        <ImageSelector itemImage={item?.api_featured_image} />
-
-        <div className={styles.productBar}>
-          <h2>{item.name}</h2>
-          <div>
-            <p className={styles.price}>
-              {item.price} {item.price_sign}
-            </p>
-            <p className={styles.description}>{item.description}</p>
-          </div>
-
-          <div className={styles.sizeIndicator}>
-            <div className={styles.selectedSize}>Size: SelectedSize</div>
-
-            <div className={styles.sizesContainer}>
-              {sizeList.map((size) => (
-                <div key={size} className={styles.size}>
-                  {size}
-                </div>
-              ))}
-            </div>
-          </div>
-
-          <div className={styles.colorIndicator}>
-            <div className={styles.selectedColor}>Color: selectedColor</div>
-            <div className={styles.colorsWrapper}>
-              {item.product_colors.slice(0, 5).map(({ hex_value }) => (
-                <div
-                  key={`${item.name}-${hex_value}`}
-                  className={styles.color}
-                  style={{ backgroundColor: hex_value }}
-                />
-              ))}
-            </div>
-          </div>
-        </div>
-
-        <div className={styles.orderBar}>
-          <ShippingOptions />
-          <QuantitySection product={item} />
-        </div>
+      <div className="flex flex-wrap box-border items-start justify-around gap-[10px] py-6 px-2">
+        <ImageSelector image={item.image} />
+        <ProductInformation item={item} />
       </div>
       <RelatedItems />
     </div>
