b686eec28047d6cfec5f658bb4fc587343b0dea8
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-06-01 20:39:07 +0300

    Add checkout route on order cart items.

--- src/app/api/product/order/route.js
+++ src/app/api/product/order/route.js
@@ -3,15 +3,22 @@ import options from "@/app/api/auth/[...nextauth]/authOptions";
 import prisma from "@/app/lib/prisma";
 
 export async function POST(req) {
-  // const { product, quantity, color } = await req.json();
-  // console.log({ product, quantity, color });
+  const { cartItems } = await req.json();
+  const orderItemsIds = cartItems.map((cartItem) => {
+    return { orderItemId: cartItem.orderItemId };
+  });
+
   const session = await getServerSession(options);
   const createdOrder = await prisma.Order.create({
     data: {
       userId: session.user.id,
       shopId: 1,
       paymentMethod: "Visa",
+      orderItems: {
+        connect: orderItemsIds,
+      },
     },
   });
+
   return Response.json(createdOrder);
 }
